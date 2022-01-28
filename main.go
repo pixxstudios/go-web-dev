719abb8fcfff7a889feb17a2ae144f5851052856
Gagandeep <gagan.ddun@gmail.com>
2022-01-28 21:16:18 +1100

    handle errors

--- main.go
+++ main.go
@@ -1,14 +1,38 @@
 package main
 
 import (
+	"errors"
 	"fmt"
 	"net/http"
 )
+
+func divide(w http.ResponseWriter, r *http.Request) {
+	f, err := divideValues(200.0, 0.0)
+
+	if err != nil {
+		fmt.Fprintf(w, fmt.Sprintf("%s", err))
+		return
+	}
+
+	fmt.Fprintf(w, fmt.Sprintf("%f", f))
+}
+
+func divideValues(x, y float32) (float32, error) {
+	if y <= 0 {
+		err := errors.New("Cannot divide by zero or negative number")
+		return 0, err
+	}
+
+	result := x / y
+	return result, nil
+}
 
 func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprintf(w, "Hello World")
 	})
+
+	http.HandleFunc("/divide", divide)
 
 	http.ListenAndServe(":8080", nil)
 }
