bc97c754d91e84fa42b21d8077e9e29e55ad8ce4
Gagandeep <gagan.ddun@gmail.com>
2022-01-28 21:53:44 +1100

    Added template

--- main.go
+++ main.go
@@ -1,38 +1,55 @@
 package main
 
 import (
-	"errors"
 	"fmt"
+	"html/template"
 	"net/http"
 )
 
-func divide(w http.ResponseWriter, r *http.Request) {
-	f, err := divideValues(200.0, 0.0)
+// func divide(w http.ResponseWriter, r *http.Request) {
+// 	f, err := divideValues(200.0, 0.0)
 
-	if err != nil {
-		fmt.Fprintf(w, fmt.Sprintf("%s", err))
-		return
-	}
+// 	if err != nil {
+// 		fmt.Fprintf(w, fmt.Sprintf("%s", err))
+// 		return
+// 	}
 
-	fmt.Fprintf(w, fmt.Sprintf("%f", f))
-}
+// 	fmt.Fprintf(w, fmt.Sprintf("%f", f))
+// }
 
-func divideValues(x, y float32) (float32, error) {
-	if y <= 0 {
-		err := errors.New("Cannot divide by zero or negative number")
-		return 0, err
-	}
+// func divideValues(x, y float32) (float32, error) {
+// 	if y <= 0 {
+// 		err := errors.New("Cannot divide by zero or negative number")
+// 		return 0, err
+// 	}
 
-	result := x / y
-	return result, nil
-}
+// 	result := x / y
+// 	return result, nil
+// }
 
 func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprintf(w, "Hello World")
 	})
 
-	http.HandleFunc("/divide", divide)
+	http.HandleFunc("/home", Home)
+
+	// http.HandleFunc("/divide", divide)
 
 	http.ListenAndServe(":8080", nil)
+}
+
+func Home(w http.ResponseWriter, r *http.Request) {
+	renderTemplate(w, "home.page.tmpl")
+}
+
+func renderTemplate(w http.ResponseWriter, tmpl string) {
+	parsedTemplate, _ := template.ParseFiles("./templates/" + tmpl)
+
+	err := parsedTemplate.Execute(w, nil)
+
+	if err != nil {
+		fmt.Println("Error parsing template ", err)
+		return
+	}
 }
